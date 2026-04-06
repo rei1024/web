27e59d498dc86aacd6af9c3e592c1d5b6c07a0c1
rei1024 <satopen1729@gmail.com>
2026-04-07 01:37:34 +0900

    feat: ウィンドウサイズ確認

---
+++ public/web/tool/window-size/index.js
@@ -0,0 +1,28 @@
+const $output = document.querySelector("#output");
+function update() {
+  $output.textContent = `Window
+  幅  : ${window.innerWidth}px
+  高さ: ${window.innerHeight}px
+
+Screen
+  利用可能幅   : ${window.screen.availWidth}
+  利用可能高さ : ${window.screen.availHeight}
+  幅           : ${window.screen.width}
+  高さ         : ${window.screen.height}
+  
+  色深度: ${window.screen.colorDepth}
+  ビット深度: ${window.screen.pixelDepth}
+  向き: ${window.screen.orientation.type}
+  角度: ${window.screen.orientation.angle}`;
+}
+// window.screen.addEventListener("");
+window.addEventListener("resize", () => {
+  update();
+});
+window.screen.addEventListener("change", () => {
+  update();
+});
+window.screen.orientation.addEventListener("change", () => {
+  update();
+});
+update();

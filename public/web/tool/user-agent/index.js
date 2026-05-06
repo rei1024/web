1bbe5b29645c2eeb0b234a515501afd20152301a
rei1024 <satopen1729@gmail.com>
2026-05-06 18:45:11 +0900

    feat: getHighEntropyValues

--- public/web/tool/user-agent/index.js
+++ public/web/tool/user-agent/index.js
@@ -1,10 +1,32 @@
 const $output = document.querySelector("#output");
 
 function update() {
+  // window.navigator.;
   $output.textContent = `userAgent = ${window.navigator.userAgent}
 
 userAgentData = ${window.navigator.userAgent ? JSON.stringify(window.navigator.userAgentData, null, 2) : "Not supported"}
 `.trim();
+
+  if (
+    window.navigator.userAgentData &&
+    window.navigator.userAgentData.getHighEntropyValues
+  ) {
+    navigator.userAgentData
+      .getHighEntropyValues([
+        "architecture",
+        "bitness",
+        "formFactor",
+        "fullVersionList",
+        "model",
+        "platformVersion",
+        "wow64",
+      ])
+      .then((values) => {
+        $output.textContent += `\n
+High entropy values = ${JSON.stringify(values, null, 2)}
+`.trimEnd();
+      });
+  }
 }
 
 update();
